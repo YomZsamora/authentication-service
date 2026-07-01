93cedbe4293229e413e63d9b8f1af6e8aefa258d
yomZsamora <samaurah@gmail.com>
2026-07-01 21:04:12 +0300

    add initial server setup with Express and dotenv

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const { exceptionHandler } = require('./utils/exceptions/exception-handler');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.get('/health', (req, res) => res.send('The authentication service is running.'));
+app.use(exceptionHandler);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+module.exports = app;
