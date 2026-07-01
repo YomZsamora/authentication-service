02d713b0762763903004000553a3cf2074e21b7a
yomZsamora <samaurah@gmail.com>
2026-07-01 23:58:55 +0300

    feat(index.js): add JWKS routes for improved authentication handling

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cookieParser = require('cookie-parser');
+const jwksRoutes = require('./app/routes/jwks-routes');
 const { exceptionHandler } = require('./utils/exceptions/exception-handler');
 
 dotenv.config();
@@ -11,6 +12,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cookieParser());
 app.get('/health', (req, res) => res.send('The authentication service is running.'));
+app.use('/.well-known', jwksRoutes);
 app.use(exceptionHandler);
 
 if (require.main === module) {
