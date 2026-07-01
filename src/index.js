eb15f28d0be36b8709eee13b6c697cf4e3ecb92a
yomZsamora <samaurah@gmail.com>
2026-07-01 23:08:57 +0300

    feat: add cookie-parser middleware for enhanced request handling

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
 const { exceptionHandler } = require('./utils/exceptions/exception-handler');
 
 dotenv.config();
@@ -8,6 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cookieParser());
 app.get('/health', (req, res) => res.send('The authentication service is running.'));
 app.use(exceptionHandler);
 
