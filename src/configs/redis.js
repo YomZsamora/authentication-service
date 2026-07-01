9900ec87d3d1c769390c097f0e6407f7065809a0
yomZsamora <samaurah@gmail.com>
2026-07-01 22:44:15 +0300

    fix(redis.js): correct Redis configuration path for app settings

--- src/configs/redis.js
+++ src/configs/redis.js
@@ -1,8 +1,8 @@
 const Redis = require('ioredis');
 const env = process.env.NODE_ENV || 'development';
-const config = require('./config')[env];
+const config = require('./config');
 
-const redis = new Redis(config.REDIS_URL || 'redis://localhost:6379');
+const redis = new Redis(config.app.REDIS_URL || 'redis://localhost:6379');
 
 redis.on('connect', () => console.log(`Connected to Redis successfully in ${env} environment.`));
 redis.on('error', (err) => console.error(`Error connecting to Redis in ${env} environment:`, err));
