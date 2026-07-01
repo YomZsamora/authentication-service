cbbd69f7ade44dd4d2b49d544f3bd817e14a52f3
yomZsamora <samaurah@gmail.com>
2026-07-01 21:04:17 +0300

    add configuration file for database connection settings

---
+++ src/configs/config.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const fs = require('fs');
+
+module.exports = {
+    development: {
+        username: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD,
+        database: process.env.POSTGRES_DATABASE,
+        host: process.env.POSTGRES_HOST,
+        dialect: 'postgres',
+    },
+    staging: {},
+    production: {},
+    test: {},
+    app: {},
+};
