dcccfff0751ae65bbc25f5221ce0bcc58e13f542
yomZsamora <samaurah@gmail.com>
2026-07-01 22:44:07 +0300

    refactor config.js: update test environment configuration and fix JWT key paths

--- src/configs/config.js
+++ src/configs/config.js
@@ -1,5 +1,4 @@
 require('dotenv').config();
-const fs = require('fs');
 
 module.exports = {
     development: {
@@ -11,10 +10,17 @@ module.exports = {
     },
     staging: {},
     production: {},
-    test: {},
+    test: {
+        username: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD,
+        database: process.env.POSTGRES_DATABASE,
+        host: process.env.POSTGRES_HOST,
+        dialect: 'postgres',
+    },
     app: {
         REDIS_URL: process.env.REDIS_URL,
-        JWT_PRIVATE_KEY: process.env.JWT_PRIVATE_KEY,
+        JWT_PRIVATE_KEY_PATH: process.env.JWT_PRIVATE_KEY_PATH,
+        JWT_PUBLIC_KEY_PATH: process.env.JWT_PUBLIC_KEY_PATH,
         JWT_ISSUER: process.env.JWT_ISSUER,
         JWT_AUDIENCE: process.env.JWT_AUDIENCE,
         JWT_KEY_ID: process.env.JWT_KEY_ID,
