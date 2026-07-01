af02bb572c4d4b1c8f237755770f0d5a462ad7f9
yomZsamora <samaurah@gmail.com>
2026-07-01 22:45:40 +0300

    refactor(index.js): reorganize imports and update middleware usage

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
 const { exceptionHandler } = require('./utils/exceptions/exception-handler');
+const authRoutes = require('./app/routes/auth.routes');
+const jwksRoutes = require('./app/routes/jwks.routes');
 
 dotenv.config();
 
@@ -8,11 +11,16 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cookieParser());
 app.get('/health', (req, res) => res.send('The authentication service is running.'));
+app.use('/auth', authRoutes);
+app.use('/', jwksRoutes);
 app.use(exceptionHandler);
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+if (require.main === module) {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+}
 
 module.exports = app;
