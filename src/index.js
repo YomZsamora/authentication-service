a321673f2ad8019c80c453e2288caf856a6914c7
yomZsamora <samaurah@gmail.com>
2026-07-01 22:45:46 +0300

    refactor(index.js): remove unused middleware and routes for cleaner code

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const cookieParser = require('cookie-parser');
 const { exceptionHandler } = require('./utils/exceptions/exception-handler');
-const authRoutes = require('./app/routes/auth.routes');
-const jwksRoutes = require('./app/routes/jwks.routes');
 
 dotenv.config();
 
@@ -11,16 +8,11 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(cookieParser());
 app.get('/health', (req, res) => res.send('The authentication service is running.'));
-app.use('/auth', authRoutes);
-app.use('/', jwksRoutes);
 app.use(exceptionHandler);
 
-if (require.main === module) {
-    app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
-    });
-}
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
 
 module.exports = app;
