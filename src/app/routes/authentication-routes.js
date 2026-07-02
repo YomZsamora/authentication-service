4742b893f54916bd42f171e555e6a28264a8b700
yomZsamora <samaurah@gmail.com>
2026-07-02 11:02:02 +0300

    feat(routes): add refresh token route to authentication routes

--- src/app/routes/authentication-routes.js
+++ src/app/routes/authentication-routes.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const { 
     basicRegistrationMiddleware, 
-    basicLoginMiddleware 
+    basicLoginMiddleware,
+    refreshTokenMiddleware
 } = require('../middlewares/authentication-middlewares');
 const { 
     basicRegistrationController, 
-    basicLoginController 
+    basicLoginController,
+    refreshTokenController
 } = require('../controllers/authentication-controller');
 
 const router = express.Router();
 
 router.post('/basic-registration', basicRegistrationMiddleware, basicRegistrationController);
 router.post('/basic-login', basicLoginMiddleware, basicLoginController);
+router.post('/refresh-token', refreshTokenMiddleware, refreshTokenController);
 
 module.exports = router;
