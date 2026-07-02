835731292c1862aed4c230d59d764d78cf5074d3
yomZsamora <samaurah@gmail.com>
2026-07-02 06:19:49 +0300

    feat(migrations): create users table with necessary fields

---
+++ src/migrations/20260702024335-create-users-model.js
@@ -0,0 +1,37 @@
+'use strict';
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.createTable('users', {
+            id: {
+                type: Sequelize.UUID,
+                defaultValue: Sequelize.UUIDV4,
+                primaryKey: true,
+            },
+            email: {
+                type: Sequelize.STRING,
+                allowNull: false,
+                unique: true,
+            },
+            password: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            role: {
+                type: Sequelize.STRING,
+                allowNull: false,
+                defaultValue: 'user',
+            },
+            createdAt: {
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
+            updatedAt: {
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
+        });
+    },
+    down: async (queryInterface) => {
+        await queryInterface.dropTable('users');
+    },
+};
