b564a4a29758d9d7a636405fae89c937ddfabf7c
yomZsamora <samaurah@gmail.com>
2026-07-02 08:40:34 +0300

    fix(routes): update import path for basicRegistrationController in authentication-routes

--- src/app/routes/authentication-routes.js
+++ src/app/routes/authentication-routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { basicRegistrationMiddleware } = require('../middlewares/authentication-middlewares');
-const { basicRegistrationController } = require('../controllers/basic-registration-controller');
+const { basicRegistrationController } = require('../controllers/authentication-controller');
 
 const router = express.Router();
 
