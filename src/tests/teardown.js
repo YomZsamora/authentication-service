d92ce65040bf044e8d1fc4989ecce79282b0fbcf
yomZsamora <samaurah@gmail.com>
2026-07-08 09:44:12 +0300

    refactor(teardown): streamline database teardown process and improve error handling

--- src/tests/teardown.js
+++ src/tests/teardown.js
@@ -1,27 +1,28 @@
+'use strict';
 const { Sequelize } = require('sequelize');
 const { test } = require('../configs/config');
 
 module.exports = async () => {
-    const adminDatabase = 'postgres'; 
-    const sequelize = new Sequelize(adminDatabase, test.username, test.password, {
+
+    const adminSequelize = new Sequelize('postgres', test.username, test.password, {
         host: test.host,
-        dialect: test.dialect,
+        dialect: 'postgres',
+        logging: false,
     });
-    
+
     try {
-        await sequelize.authenticate();
-        await new Promise(resolve => setTimeout(resolve, 3000)); 
-        await sequelize.query(`
+        await adminSequelize.authenticate();
+        await adminSequelize.query(`
             SELECT pg_terminate_backend(pg_stat_activity.pid)
             FROM pg_stat_activity
             WHERE pg_stat_activity.datname = '${test.database}'
-            AND pid <> pg_backend_pid();
+            AND pid <> pg_backend_pid()
         `);
-        await sequelize.query(`DROP DATABASE IF EXISTS "${test.database}"`);
-        console.log(`Database ${test.database} dropped.`);
+        await adminSequelize.query(`DROP DATABASE IF EXISTS "${test.database}"`);
+        console.log(`Test database "${test.database}" dropped.`);
     } catch (error) {
-        console.error('Unable to connect to the database:', error);
+        console.error('Error during teardown:', error);
     } finally {
-        await sequelize.close();
+        await adminSequelize.close();
     }
 };
