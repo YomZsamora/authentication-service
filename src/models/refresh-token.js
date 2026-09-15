a5db1e6a782de2a7d51e9a6e7943e4163e4dfb65
yomZsamora <samaurah@gmail.com>
2026-09-15 21:35:17 +0300

    refactor(refresh-token-model): standardize formatting and improve code consistency by organizing model definition and adding missing commas

--- src/models/refresh-token.js
+++ src/models/refresh-token.js
@@ -1,40 +1,43 @@
 const { DataTypes } = require('sequelize');
-const sequelize = require('../configs/sequelize'); 
+const sequelize = require('../configs/sequelize');
 
-const RefreshToken = sequelize.define('RefreshToken', {
-
-    id: {
-        type: DataTypes.UUID,
-        defaultValue: DataTypes.UUIDV4,
-        primaryKey: true,
-    },
-    jti: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-    },
-    userId: {
-        type: DataTypes.UUID,
-        allowNull: true,
-        references: { model: 'users', key: 'id' },
-        onDelete: 'SET NULL',
-    },
-    expiryDate: {
-        type: DataTypes.DATE,
-        allowNull: false,
-    },
-}, {
-    tableName: 'refresh_tokens',
-    indexes: [
-        {
-            name: 'idx_refresh_tokens_userId',
-            fields: ['userId']
+const RefreshToken = sequelize.define(
+    'RefreshToken',
+    {
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            primaryKey: true,
         },
-        {
-            name: 'idx_refresh_tokens_jti',
-            fields: ['jti']
-        }
-    ]
-});
+        jti: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+        userId: {
+            type: DataTypes.UUID,
+            allowNull: true,
+            references: { model: 'users', key: 'id' },
+            onDelete: 'SET NULL',
+        },
+        expiryDate: {
+            type: DataTypes.DATE,
+            allowNull: false,
+        },
+    },
+    {
+        tableName: 'refresh_tokens',
+        indexes: [
+            {
+                name: 'idx_refresh_tokens_userId',
+                fields: ['userId'],
+            },
+            {
+                name: 'idx_refresh_tokens_jti',
+                fields: ['jti'],
+            },
+        ],
+    }
+);
 
 module.exports = { RefreshToken };
