c53c30deb6045971b61ae39ac04765f05be96c10
yomZsamora <samaurah@gmail.com>
2026-09-15 21:18:01 +0300

    refactor(migrations): update migration to use consistent parameter naming in up and down methods

--- src/migrations/20260810000000-drop-redundant-googleSub-unique-constraint.js
+++ src/migrations/20260810000000-drop-redundant-googleSub-unique-constraint.js
@@ -2,11 +2,11 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-    async up(queryInterface, Sequelize) {
+    async up(queryInterface, _Sequelize) {
         await queryInterface.removeConstraint('users', 'users_googleSub_key');
     },
 
-    async down(queryInterface, Sequelize) {
+    async down(queryInterface, _Sequelize) {
         await queryInterface.addConstraint('users', {
             fields: ['googleSub'],
             type: 'unique',
