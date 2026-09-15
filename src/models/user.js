e1e94e12a4582b30bf17259147ab465154d26a20
yomZsamora <samaurah@gmail.com>
2026-09-15 21:35:21 +0300

    refactor(user-model): standardize formatting and improve code consistency by organizing model definition and ensuring proper indentation

--- src/models/user.js
+++ src/models/user.js
@@ -3,54 +3,57 @@ const bcrypt = require('bcryptjs');
 const sequelize = require('../configs/sequelize');
 const roles = ['ADMIN', 'USER', 'VENUE', 'PERFORMER'];
 
-const User = sequelize.define('User', {
+const User = sequelize.define(
+    'User',
+    {
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            primaryKey: true,
+        },
 
-    id: {
-        type: DataTypes.UUID,
-        defaultValue: DataTypes.UUIDV4,
-        primaryKey: true,
-    },
-
-    email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-        validate: { isEmail: true },
-    },
-
-    password: {
-        type: DataTypes.STRING,
-        allowNull: true,
-    },
-
-    googleSub: {
-        type: DataTypes.STRING,
-        allowNull: true,
-        unique: true,
-    },
-    
-    role: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        defaultValue: 'user',
-        validate: { isIn: [roles] }
-    },
-}, {
-    tableName: 'users',
-    indexes: [
-        {
-            name: 'idx_users_googleSub',
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
             unique: true,
-            fields: ['googleSub'],
-        }
-    ],
-    hooks: {
-        beforeCreate: (user) => {
-            if (user.email) user.email = user.email.trim().toLowerCase();
-            if (user.password) user.password = hashPassword(user.password);
-        }
+            validate: { isEmail: true },
+        },
+
+        password: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+
+        googleSub: {
+            type: DataTypes.STRING,
+            allowNull: true,
+            unique: true,
+        },
+
+        role: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            defaultValue: 'user',
+            validate: { isIn: [roles] },
+        },
+    },
+    {
+        tableName: 'users',
+        indexes: [
+            {
+                name: 'idx_users_googleSub',
+                unique: true,
+                fields: ['googleSub'],
+            },
+        ],
+        hooks: {
+            beforeCreate: (user) => {
+                if (user.email) user.email = user.email.trim().toLowerCase();
+                if (user.password) user.password = hashPassword(user.password);
+            },
+        },
     }
-});
+);
 
 const hashPassword = (password) => {
     const saltRounds = 10;
